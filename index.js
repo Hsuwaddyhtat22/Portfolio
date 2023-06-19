5bf5b7b111df4b716bfaffbd2b894abc748a8c8f
blessing-michael <blessingmike334@gmail.com>
2023-06-19 15:34:24 -0700

    Add javascript functionality to index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const Favicon = document.querySelector('.hamburger');
-const links = document.querySelector('.nav-ham');
+const links = document.querySelectorAll('.nav-ham');
 const linksContainer = document.querySelector('.earth');
+const closeTag = document.querySelector('.close-tag');
 Favicon.addEventListener('click', () => {
-    console.log('clicked');
-    links.classList.toggle('active')
+    console.log('clicked favicon');
+    linksContainer.classList.add('active')
 });
+
+closeTag.addEventListener('click', ()=>{
+    console.log('clicked closetag')
+    linksContainer.classList.remove('active')
+})
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        linksContainer.classList.remove('active')
+        console.log(link)
+    //   closeMenu();
+    });
+  });
