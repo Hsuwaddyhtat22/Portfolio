b627574012aba36e361b20ce5926ca412bbf5334
blessing-michael <blessingmike334@gmail.com>
2023-06-19 15:37:12 -0700

    Update javascript file

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const links = document.querySelectorAll('.nav-ham');
 const linksContainer = document.querySelector('.earth');
 const closeTag = document.querySelector('.close-tag');
 Favicon.addEventListener('click', () => {
-    console.log('clicked favicon');
     linksContainer.classList.add('active')
 });
 
 closeTag.addEventListener('click', ()=>{
-    console.log('clicked closetag')
+
     linksContainer.classList.remove('active')
 })
 links.forEach((link) => {
     link.addEventListener('click', () => {
         linksContainer.classList.remove('active')
-        console.log(link)
-    //   closeMenu();
+     
     });
   });
