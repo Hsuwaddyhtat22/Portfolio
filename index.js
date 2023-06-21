ebb1ee3ff67d9da5950804dcfab1e8c9ca8ac262
Hsu Waddy <hsuwady2001@gmail.com>
2023-06-21 13:48:14 +0630

    add-mobilemenu

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const ham = document.querySelector(".hamburger");
 const links = document.querySelector(".earth");
-
-const closetag = document.querySelector(".close-tag");
-const navham = document.querySelectorAll(".nav-ham");
-
+const closemenu = document.querySelector(".close-tag");
+const navham = document.querySelectorAll(".nav-hamClose");
+const menuOptions = document.querySelectorAll(".nav-hamClose li a");
+const closetag = document.querySelector(".close-tag img");
 ham.addEventListener("click", () => {
 
     links.classList.toggle("earthClose")
 })
 closetag.addEventListener("click", () => {
+    links.classList.add("earthClose");
+});
+closetag.addEventListener("click", () => {
+    links.style.display = "none";
+});
 
-    links.classList.toggle("earthClose")
-})
+menuOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+        links.classList.add("earthClose");
+    });
+});
+
+links.style.display = "none";
+
+ham.addEventListener("click", () => {
+    if (links.style.display === "none") {
+        links.style.display = "flex";
+    } else {
+        links.style.display = "none";
+    }
+});
+
+menuOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+
+        links.style.display = "none";
+    });
+});
