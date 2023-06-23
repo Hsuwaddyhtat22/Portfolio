ca24f7e2f5b854f2f92251bad840cb586c09a6f4
Hsu Waddy <hsuwady2001@gmail.com>
2023-06-23 21:54:48 +0630

    Add-Form-Collaboartion

--- index.js
+++ index.js
@@ -175,3 +175,21 @@ allNavlinks.forEach((link) => {
     closeMenu();
   });
 });
+function errorMessage() {
+  if (email.value !== email.value.toLowerCase()) {
+    error.textContent = 'Please ensure that email address must be in lower case';
+  }
+}
+
+email.addEventListener('input', () => {
+  if (email.value === email.value.toLowerCase()) {
+    error.textContent = '';
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  if (email.value !== email.value.toLowerCase()) {
+    event.preventDefault();
+    errorMessage();
+  }
+});
